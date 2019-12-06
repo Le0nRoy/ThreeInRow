cc65703e60285795268dcc7f339a1f3c3a3f1be3
le0n <cerber9283@gmail,com>
2019-12-06 15:54:11 +0300

    Updated deleting
    By some reason deletes if two in row, not delete more than 2 to one side. No filling of empty.

--- ThreeInRow/src/ThreeInRowGame.cpp
+++ ThreeInRow/src/ThreeInRowGame.cpp
@@ -17,28 +17,36 @@ std::string clickNewKey( "Для продолжения нажмите любую клавишу!" );
 #endif
 
 ThreeInRowGame::ThreeInRowGame( ) : 
+    fieldSize( 10 ),
     gameIsRunning( false ),
     rowNum( 0 ),
     colNum( 0 ),
     moveDirection( 0 ),
     score( 0 )
 {
-    screenBuf.resize( 10 );
-    /*std::for_each( screenBuf.begin( ), screenBuf.end( ), []( std::string val )
-        {
-            val.resize( 10 );
-        } );*/
-    screenBuf.at( 0 ) = "AFIAAAAAAA";
-    screenBuf.at( 1 ) = "DBGAAAAAAA";
-    screenBuf.at( 2 ) = "ECHAAAAAAA";
-    screenBuf.at( 3 ) = "AAAAAAAAAA";
-    screenBuf.at( 4 ) = "AAAAAAAAAA";
-    screenBuf.at( 5 ) = "AAAAAAAAAA";
-    screenBuf.at( 6 ) = "AAAAAAAAAA";
-    screenBuf.at( 7 ) = "AAAAAAAAAA";
-    screenBuf.at( 8 ) = "AAAAAAAAAA";
-    screenBuf.at( 9 ) = "AAAAAAAAAA";
+    screenBuf.resize( fieldSize );
+    for ( int i = 0; i < fieldSize; i++ )
+    {
+        screenBuf.at( i ).assign( fieldSize, ' ' );
+    }
+
     randomBuf( );
+}
+
+void ThreeInRowGame::randomBuf( )
+{
+    srand( time( NULL ) );
+    std::for_each( screenBuf.begin( ), screenBuf.end( ), []( std::string &val )
+        {
+            std::for_each( val.begin( ), val.end( ), []( char &c )
+                {
+                    if ( c == ' ' )
+                    {
+                        char addable = static_cast<char>( rand( ) % 6 );
+                        c = 'A' + addable;
+                    }
+                } );
+        } );
 }
 
 void ThreeInRowGame::startGame( )
@@ -53,7 +61,7 @@ void ThreeInRowGame::paintWwindow( )
     std::cout << " Приветствую! Вы играете в игру 3 в ряд." << std::endl;
     std::cout << " Правила игры. Вам нужно совместить 3 одинаковых буквы в один ряд." << std::endl;
     std::cout << "Данный блок удалится. Вам необходимо соединять таким образом блоки" << std::endl;
-    std::cout << "до тех пор, пока не будет очищено все поле." << std::endl;
+    std::cout << "до тех пор, пока вы не наберете 100 очков." << std::endl;
     std::cout << " Правила игры.";
     std::cout << "Для перемещения элемента необходимо ввести команду типа <MRCD>,";
     std::cout << "где R - номер ряда, C - номер столбца, D - направление перемещения.";
@@ -110,15 +118,45 @@ int ThreeInRowGame::parseCommand( )
         ( direction == left || direction == right || direction == down || direction == up )
         )
     {
-        rowNum = row - '0';
-        colNum = col - '0';
-        moveDirection = direction;
-        if ( rowNum == 0 && moveDirection == 'u' ||
-            rowNum == 9 && moveDirection == 'd' ||
-            colNum == 0 && moveDirection == 'l' ||
-            colNum == 9 && moveDirection == 'r' )
+        prevRowNum = row - '0';
+        prevColNum = col - '0';
+        if ( prevRowNum == 0 && moveDirection == 'u' ||
+            prevRowNum == 9 && moveDirection == 'd' ||
+            prevColNum == 0 && moveDirection == 'l' ||
+            prevColNum == 9 && moveDirection == 'r' )
         {
             return -1;
+        }
+        moveDirection = direction;
+
+        switch ( moveDirection )
+        {
+        case 'l':
+        {
+            colNum = prevColNum - 1;
+            rowNum = prevRowNum;
+            break;
+        }
+        case 'r':
+        {
+            colNum = prevColNum + 1;
+            rowNum = prevRowNum;
+            break;
+        }
+        case 'u':
+        {
+            rowNum = prevRowNum - 1;
+            colNum = prevColNum;
+            break;
+        }
+        case 'd':
+        {
+            rowNum = prevRowNum + 1;
+            colNum = prevColNum;
+            break;
+        }
+        default:
+            break;
         }
         return 0;
     }
@@ -127,52 +165,219 @@ int ThreeInRowGame::parseCommand( )
 
 void ThreeInRowGame::changeObjects( )
 {
-    char secRow = 0;
-    char secCol = 0;
-    if ( moveDirection == 'u' )
-    {
-        secRow = rowNum - 1;
-    }
-    else if ( moveDirection == 'd' )
-    {
-        secRow = rowNum + 1;
-    }
-    else
-    {
-        secRow = rowNum;
-    }
-
-    if ( moveDirection == 'l' )
-    {
-        secCol = colNum - 1;
-    }
-    else if ( moveDirection == 'r' )
-    {
-        secCol = colNum + 1;
-    }
-    else
-    {
-        secCol = colNum;
-    }
-    char &firstObject = screenBuf[ rowNum ][ colNum ];
-    char &secondObject = screenBuf[ secRow ][ secCol ];
+    char &firstObject = screenBuf[ prevRowNum ][ prevColNum ];
+    char &secondObject = screenBuf[ rowNum ][ colNum ];
 
     char buf = firstObject;
     firstObject = secondObject;
     secondObject = buf;
 }
 
-void ThreeInRowGame::randomBuf(  )
+bool ThreeInRowGame::checkThreeInRow( )
 {
-    srand( time( NULL ) );
-    std::for_each( screenBuf.begin( ), screenBuf.end( ), []( std::string &val )
+    switch ( moveDirection )
+    {
+    case 'l' :
+    {
+        int up = checkSide( 'u' );
+        int down = checkSide( 'd' );
+        int left = checkSide( 'l' );
+        if ( up + down > 2 || ( up + down == 2 && left < 2 ) )
         {
-            std::for_each( val.begin( ), val.end( ), []( char &c )
-                {
-                    char addable = static_cast<char>( rand( ) % 6 );
-                    c = 'A' + addable;
-                } );
-        } );
+            deleteRow( 'u', up, down );
+        }
+        else if ( left == 2 )
+        {
+            deleteRow( 'l', left, 0 );
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+        break;
+    }
+    case 'r':
+    {
+        int up = checkSide( 'u' );
+        int down = checkSide( 'd' );
+        int right = checkSide( 'r' );
+        if ( up + down > 2 || ( up + down == 2 && right < 2 ) )
+        {
+            deleteRow( 'u', up, down );
+        }
+        else if ( right == 2 )
+        {
+            deleteRow( 'r', right, 0 );
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+        break;
+    }
+    case 'u':
+    {
+        int up = checkSide( 'u' );
+        int right = checkSide( 'r' );
+        int left = checkSide( 'l' );
+        if ( right + left > 2 || ( right + left == 2 && up < 2 ) )
+        {
+            deleteRow( 'r', right, left );
+        }
+        else if ( up == 2 )
+        {
+            deleteRow( 'u', up, 0 );
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+        break;
+    }
+    case 'd':
+    {
+        int right = checkSide( 'r' );
+        int down = checkSide( 'd' );
+        int left = checkSide( 'l' );
+        if ( right + left > 2 || ( right + left == 2 && down < 2 ) )
+        {
+            deleteRow( 'r', right, left );
+        }
+        if ( down == 2 )
+        {
+            deleteRow( 'd', down, 0 );
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+        break;
+    }
+    default:
+        break;
+    }
+    return false;
+}
+
+int ThreeInRowGame::checkSide( char side )
+{
+    char curChar = screenBuf[ rowNum ][ colNum ];
+    int counter = 0;
+    switch ( side )
+    {
+    case 'l':
+        if ( colNum != 0 && curChar == screenBuf[ rowNum ][ colNum - 1 ] )
+        {
+            counter = 1;
+            if ( colNum != 1 && curChar == screenBuf[ rowNum ][ colNum - 2 ] )
+            {
+                counter = 2;
+            }
+        }
+        return counter;
+        break;
+    case 'r':
+        if ( colNum != fieldSize - 1 && curChar == screenBuf[ rowNum ][ colNum + 1 ] )
+        {
+            counter = 1;
+            if ( colNum != fieldSize - 2 && curChar == screenBuf[ rowNum ][ colNum + 2 ] )
+            {
+                counter = 2;
+            }
+        }
+        return counter;
+        break;
+    case 'u':
+        if ( rowNum != 0 && curChar == screenBuf[ rowNum - 1 ][ colNum ] )
+        {
+            counter = 1;
+            if ( colNum != 1 && curChar == screenBuf[ rowNum - 2 ][ colNum ] )
+            {
+                counter = 2;
+            }
+        }
+        return counter;
+        break;
+    case 'd':
+        if ( rowNum != fieldSize - 1 && curChar == screenBuf[ rowNum + 1 ][ colNum ] )
+        {
+            counter = 1;
+            if ( rowNum != fieldSize - 2 && curChar == screenBuf[ rowNum + 2 ][ colNum ] )
+            {
+                counter = 2;
+            }
+        }
+        return counter;
+        break;
+    default:
+        return -1;
+        break;
+    }
+    return 0;
+}
+
+void ThreeInRowGame::deleteRow( char side, int num1, int num2 )
+{
+    switch ( side )
+    {
+    case 'l':
+    {
+        screenBuf.at( rowNum ).at( colNum ) = ' ';
+        for ( int i = 0; i <= num1; i++ )
+        {
+            screenBuf.at( rowNum ).at( colNum - i ) = ' ';
+        }
+        for ( int i = 0; i <= num2; i++ )
+        {
+            screenBuf.at( rowNum ).at( colNum + i ) = ' ';
+        }
+        break;
+    }
+    case 'r':
+    {
+        screenBuf.at( rowNum ).at( colNum ) = ' ';
+        for ( int i = 0; i <= num1; i++ )
+        {
+            screenBuf.at( rowNum ).at( colNum + i ) = ' ';
+        }
+        for ( int i = 0; i <= num2; i++ )
+        {
+            screenBuf.at( rowNum ).at( colNum - i ) = ' ';
+        }
+        break;
+    }
+    case 'u':
+    {
+        screenBuf.at( rowNum ).at( colNum ) = ' ';
+        for ( int i = 0; i <= num1; i++ )
+        {
+            screenBuf.at( rowNum - i ).at( colNum ) = ' ';
+        }
+        for ( int i = 0; i <= num2; i++ )
+        {
+            screenBuf.at( rowNum + i ).at( colNum ) = ' ';
+        }
+        break;
+    }
+    case 'd':
+    {
+        screenBuf.at( rowNum ).at( colNum ) = ' ';
+        for ( int i = 0; i <= num1; i++ )
+        {
+            screenBuf.at( rowNum + i ).at( colNum ) = ' ';
+        }
+        for ( int i = 0; i <= num2; i++ )
+        {
+            screenBuf.at( rowNum - i ).at( colNum ) = ' ';
+        }
+        break;
+    }
+    }
+
 }
 
 void ThreeInRowGame::runCycle( )
@@ -199,6 +404,10 @@ void ThreeInRowGame::runCycle( )
         changeObjects( );
         paintWwindow( );
         PAUSE;
+        if ( checkThreeInRow( ) )
+        {
+            //randomBuf( );
+        }
 
         system( clearConsole.c_str( ) );
     }
--- ThreeInRow/src/ThreeInRowGame.h
+++ ThreeInRow/src/ThreeInRowGame.h
@@ -21,12 +21,20 @@ private:
     void randomBuf(  );
     void changeObjects( );
     // осталось прописать првоерку на "3 в ряд" при создании окна и при каждой итерации цикла
+    int checkSide( char side );
+    bool checkThreeInRow( );
+    void deleteRow( char side, int num1, int num2 = 0 );
 
 private:
     std::vector<std::string> screenBuf;
     std::string command;
+
+    int fieldSize;
+
     bool gameIsRunning;
 
+    int prevRowNum;
+    int prevColNum;
     int rowNum;
     int colNum;
     char moveDirection;
